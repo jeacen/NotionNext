746a3e8f35c0f6f7c59fc3e0751f4a30916c7734
jeacen <jeacen@jeacen.com>
2024-07-09 21:02:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,14 +20,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是Jeacen, 欢迎来到我的博客🎉',
+    'Hi，欢迎来到我的ichaintalk！🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jeacen', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '学无止境，财随智增🎉', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://jeacen.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, Jeacen', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ichaintalk.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, ichaintalk, 挖矿, 区块链, 矿工, 矿池, 虚拟货币, 比特币', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
