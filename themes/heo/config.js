92e66a6cec0a41105c6c2f0e4ce42b9e19203806
jeacen <jeacen@jeacen.com>
2024-07-10 00:33:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -26,9 +26,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://ichaintalk.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '挖矿学院', url: '/tag/挖矿学院' },
+  HEO_HERO_CATEGORY_2: { title: '云计算学院', url: '/tag/云计算学院' },
+  HEO_HERO_CATEGORY_3: { title: '精彩分享', url: '/tag/精彩分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
